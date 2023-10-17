75110f7a5b0ef71e2e3494826ed1d84e96024345
hasibul-99 <hasibul_uap@outlook.com>
2023-10-17 22:22:24 +0600

    landing page full overview

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
-    // px-4, py-4 = p-4
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
